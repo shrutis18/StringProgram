5bf10b7067693cdbb12d5c7ffcd52ec752aa886b
shrutis18 <ssingh@equalexperts.com>
2016-08-23 00:26:44 +0530

    Add files via upload

--- Stringprob.java
+++ Stringprob.java
@@ -1,37 +1,97 @@
 
 
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.Scanner;
 
 class Stringprob {
 
 
-    public static void main (String args[]) {
-        String str = "I am a Boy I am a a";
-        String[] splitStr = str.split(" ");
-        int count = 0;
-        List<String> list = new ArrayList<>();
-        for (String s : splitStr) {
-            if (!list.contains(s)) {
-                list.add(s);
-            }
-        }
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < splitStr.length; j++) {
-                if (list.get(i).equals(splitStr[j])) {
-                    count++;
-                }
-            }
+    public static void main(String args[]) throws IOException {
+        String filename = getFileName();
+        int cutoff = getThreshold();
 
-                System.out.println("Occurrence of " + list.get(i) + " is " + count + " times.");
-                count = 0;
+        WordCount sb = new WordCount();
+        sb.wordCount(filename);
+        sb.display(cutoff);
 
-        }
     }
 
-}
+    public static String getFileName() throws IOException {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the name of your file .... ");
+        String fileName;
+        File file;
+        while (true) {
+            fileName = reader.nextLine();
+            file = new File(fileName);
+            if (!file.exists() || fileName.isEmpty()) {
+                System.out.println("Enter A Valid File Name:");
+            } else break;
+        }
+        assert (fileName != null) : "File Name is null";
+        assert (fileName.trim().isEmpty() != true) : "File name is empty";
+        return fileName;
+    }
+
+    public static int getThreshold() {
+
+
+        Scanner reader = new Scanner(System.in);
+           int threshold;
+
+        boolean flag = true;
+                  while(true) {
+                      System.out.println("please enter threshold");
+                       threshold = reader.nextInt();
+                    int  check = Integer.valueOf(threshold);
+
+                      try {
+
+
+                          if (check <= 0) {
+
+                              System.out.println("input is not a positive Integer ");
+                              System.out.println("Please enter a positive integer seed value: ");
+                              continue;
+                          } else {
+                              System.out.println("threshold is" + threshold);
+
+                          }
+                      }
+                      catch(NumberFormatException e){
+                              System.out.println("input is not a positive Integer ");
+                              System.out.println("Please enter a positive integer seed value: ");
+
+                          }
+                          return threshold;
+                      }
+                  }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
